2319fb8523852b9e7ee66435d2ff421b4284c577
Esraa Mohamed <esraamohamed4971@gmail.com>
2025-10-30 13:25:57 +0300

    Add FSConstants and SuperblockManager

---
+++ OS_Project/FSConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Project
+{
+    internal class FSConstants
+    {
+        public const int CLUSTER_SIZE = 1024;
+        public const int CLUSTER_COUNT = 1024;
+        public const int SUPERBLOCK_CLUSTER = 0;
+        public const int FAT_START_CLUSTER = 1;
+        public const int FAT_END_CLUSTER = 10;
+        public const int CONTENT_START_CLUSTER = 11;
+        public const int ROOT_DIR_FIRST_CLUSTER = 11;
+    }
+}
---
+++ OS_Project/SuperblockManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Project
+{
+    internal class SuperblockManager
+    {
+        private readonly virtualDisk _disk;
+
+        public SuperblockManager(virtualDisk disk)
+        {
+            _disk = disk;
+
+            byte[] superblock = _disk.ReadCluster(FSConstants.SUPERBLOCK_CLUSTER);
+            bool allZero = true;
+            foreach (byte b in superblock)
+            {
+                if (b != 0)
+                {
+                    allZero = false;
+                    break;
+                }
+            }
+
+            if (!allZero)
+                return;
+
+            byte[] emptySuperblock = new byte[FSConstants.CLUSTER_SIZE];
+            _disk.WriteCluster(FSConstants.SUPERBLOCK_CLUSTER, emptySuperblock);
+        }
+
+        public byte[] ReadSuperblock()
+        {
+            return _disk.ReadCluster(FSConstants.SUPERBLOCK_CLUSTER);
+        }
+
+        public void WriteSuperblock(byte[] data)
+        {
+            if (data.Length != FSConstants.CLUSTER_SIZE)
+                throw new ArgumentException("Superblock size must be exactly 1024 bytes.");
+
+            _disk.WriteCluster(FSConstants.SUPERBLOCK_CLUSTER, data);
+        }
+    }
+}
